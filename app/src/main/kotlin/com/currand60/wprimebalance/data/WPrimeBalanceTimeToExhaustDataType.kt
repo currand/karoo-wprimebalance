a83d878cb17fbaf0221cb9fe98b17beb3a4e5983
David Curran <currand@gmail.com>
2025-08-05 21:23:06 -0400

    Add time to exhaust data type

---
+++ app/src/main/kotlin/com/currand60/wprimebalance/data/WPrimeBalanceTimeToExhaustDataType.kt
@@ -0,0 +1,67 @@
+package com.currand60.wprimebalance.data
+
+import androidx.glance.appwidget.ExperimentalGlanceRemoteViewsApi
+import com.currand60.wprimebalance.KarooSystemServiceProvider
+import io.hammerhead.karooext.extension.DataTypeImpl
+import io.hammerhead.karooext.internal.Emitter
+import io.hammerhead.karooext.models.DataPoint
+import io.hammerhead.karooext.models.DataType
+import io.hammerhead.karooext.models.StreamState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+
+@OptIn(ExperimentalAtomicApi::class, ExperimentalGlanceRemoteViewsApi::class)
+class WPrimeBalanceTimeToExhaustDataType(
+    private val karooSystem: KarooSystemServiceProvider,
+    extension: String,
+    private val calculator: WPrimeCalculator
+) : DataTypeImpl(extension, TYPE_ID) {
+
+    init {
+        Timber.d("WPrimeBalanceTimeToExhaustDataType created")
+    }
+
+    companion object {
+        const val TYPE_ID = "wprimetimetoexhaust"
+    }
+
+    private val dataScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    override fun startStream(emitter: Emitter<StreamState>) {
+        val job = dataScope.launch {
+            val wPrimeFlow = karooSystem.streamDataFlow(
+                DataType.dataTypeId(extension, WPrimeBalanceDataType.TYPE_ID)
+            )
+            wPrimeFlow
+                .map { streamState ->
+                when (streamState) {
+                    is StreamState.Streaming -> {
+                        val currentWPrimeJoules = calculator.getCurrentWPrimeJoules()
+                        val wPrimeBal = if (currentWPrimeJoules > 0) {
+                            (streamState.dataPoint.singleValue ?: 0.0) / currentWPrimeJoules * 100.0
+                        } else {
+                            Timber.w("WPrimeCalculator's current W' Joules is 0, cannot calculate percentage.")
+                            0.0 // Default to 0% or some other sensible value
+                        }
+                        StreamState.Streaming(
+                            DataPoint(
+                                dataTypeId,
+                                values = mapOf(DataType.Field.SINGLE to wPrimeBal),
+                            ),
+                        )
+                    } else -> {
+                        streamState
+                    }
+                }
+            }.collect { emitter.onNext(it) }
+        }
+        emitter.setCancellable {
+            job.cancel()
+        }
+    }
+}
