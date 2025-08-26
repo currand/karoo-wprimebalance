ff76cdbd2429006fd981676b2872171922abe5ea
David Curran <currand@gmail.com>
2025-08-26 14:00:21 -0400

    Update README.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.2.1"
         signingConfig = signingConfigs.getByName("debug")
     }
 
