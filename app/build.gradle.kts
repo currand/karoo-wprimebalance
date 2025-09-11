bf43b92d57580b3a9f2b07359e653ccb7e684f73
David Curran <currand@gmail.com>
2025-09-11 17:27:02 -0400

    Change `android:resource` to `android:value` in manifest URL
    The `android:resource` attribute in `AndroidManifest.xml` expects a resource ID, but a URL string was provided. This commit changes `android:resource` to `android:value` to correctly reference the manifest URL as a string.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.currand60.wprimebalance"
         minSdk = 23
         targetSdk = 34
-        versionCode = 25091001
+        versionCode = 25091101
         versionName = "0.2.2"
         signingConfig = signingConfigs.getByName("debug")
     }
